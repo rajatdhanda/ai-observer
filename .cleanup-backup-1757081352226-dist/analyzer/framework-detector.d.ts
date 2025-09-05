a7394ee04a966ca0fd28b2ff6c7edf427e965129
rajatdhanda <dhanda.rajat@gmail.com>
2025-09-05 22:19:38 +0530

    Clean up backup files created during refactoring - remove repository bloat

--- .cleanup-backup-1757081352226-dist/analyzer/framework-detector.d.ts
+++
@@ -1,6 +0,0 @@
-import { FrameworkInfo } from './index';
-export declare class FrameworkDetector {
-    detect(projectPath: string): Promise<FrameworkInfo>;
-    private identifyFramework;
-}
-//# sourceMappingURL=framework-detector.d.ts.map
